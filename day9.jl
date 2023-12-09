5f626d481828899d7da3ba16477e78c7b58da40a
Sikorski <sikorski@zib.de>
2023-12-09 17:00:29 +0100

    go functional

--- day9.jl
+++ day9.jl
@@ -2,35 +2,35 @@
 # where i used the ugly explicit formulation for part2
 # reverse trick spotted only afterwards
 
-function parseinput(lines)
-  map(lines) do l
-    parse.(Int, split(l))
-  end
-end
+using IterTools
+using Lazy: @>>
 
-function part1(data)
-  sum(parseinput(data) |> extrapolate)
-end
+parseline(l) = @>> split(l) parse.(Int)
+extrapolate(series) = @>> series iterated(diff) takewhile(!iszero) sum(last)
 
-function part2(data)
-  sum(parseinput(data) .|> reverse .|> extrapolate)
-end
+part1(data) = data .|> parseline .|> extrapolate |> sum
+part2(data) = data .|> parseline .|> reverse .|> extrapolate |> sum
 
-function extrapolate(series)
-  diffs = series
-  sol = 0
-  while any(diffs .!= 0)
-    sol += diffs[end]
-    diffs = diff(diffs)
-  end
-  sol
-end
+using Transducers, Folds
+# automatic parralelization via Folds :>
+# i wished @>> worked here
+part2trans(data) = data |> Map(parseline) |> Map(reverse) |> Map(extrapolate) |> Folds.sum
+#part2folds(data) = Folds.sum(parseline ⨟ reverse ⨟ extrapolate, data)
+
+#parseinput(lines) = map(l -> parse.(Int, split(l)), lines)
+#extrapolate(series) = sum(last, takewhile(!iszero, iterated(diff, series)))
+#part1(data) =  sum(extrapolate, parseinput(data))
+#part2(data) =  sum(extrapolate ∘ reverse, parseinput(data))
+
 
 ###
 
 sample = split("0 3 6 9 12 15
   1 3 6 10 15 21
   10 13 16 21 30 45", "\n")
+function test()
+  @assert part1(sample) == 114
+  @assert part2(sample) == 2
+end
 
-@assert part1(sample) == 114
-@assert part2(sample) == 2
+test()
