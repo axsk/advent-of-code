189edb6e3e244ccf8656eef31a888766d1c90e42
Sikorski <sikorski@zib.de>
2023-12-10 13:17:13 +0100

    cleanup

--- 2023/src/day_10.jl
+++ 2023/src/day_10.jl
@@ -1,33 +1,31 @@
-# https://adventofcode.com/2023/day/10
-
 input = readlines("2023/data/day_10.txt")
 parse_input(lines) = permutedims(reduce(hcat, collect.(lines)))
 
+N, E, S, W = (-1, 0), (0, 1), (1, 0), (0, -1)
 directions = Dict(
-    '|' => ((-1, 0), (1, 0)),
-    '-' => ((0, -1), (0, 1)),
-    'L' => ((-1, 0), (0, 1)),
-    'J' => ((-1, 0), (0, -1)),
-    '7' => ((1, 0), (0, -1)),
-    'F' => ((1, 0), (0, 1)),
-    'S' => ((-1, 0), (1, 0), (0, -1), (0, 1))
+    '|' => (N, S),
+    '-' => (W, E),
+    'L' => (N, E),
+    'J' => (N, W),
+    '7' => (S, W),
+    'F' => (S, E),
+    'S' => (N, S, W, E)
 )
 
 function solve(input)
     grid = parse_input(input)
     start = findfirst(grid .== 'S')
     pos = start
-    history = [start, start]
+    history = [start, start]  # still disliking this
     area = 0
     while true
-        pipe = grid[pos]
-        dirs = directions[pipe]
+        dirs = directions[grid[pos]]
         for d in dirs
             next = pos + CartesianIndex(d)
             if grid[next] in keys(directions) && history[end-1] != next
                 dy = next[1] + pos[1]
                 dx = next[2] - pos[2]
-                area += 1 / 2 * (dx) * (dy) # shoelace formula
+                area += 1 / 2 * (dx) * (dy)  # shoelace formula
                 pos = next
                 break
             end
