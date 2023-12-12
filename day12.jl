93aca9c463b59e6988a800dcec510dc363016381
Sikorski <sikorski@zib.de>
2023-12-12 16:16:42 +0100

    remove old regex code, the linear works without weird input appends

--- day12.jl
+++ day12.jl
@@ -2,7 +2,7 @@ parseinput(s::String) = parseinput(split(s, "\n"))
 function parseinput(lines)
     map(lines) do line
         pattern, counts = split(line, " ")
-        pattern = pattern * "."
+        pattern = pattern
         counts = parse.(Int, split(counts, ","))
         pattern, counts
     end
@@ -10,54 +10,59 @@ end
 
 const mem = Dict{Tuple{String,Vector{Int}},Int}()
 reset() = delete!.(Ref(mem), keys(mem))
-
-countmatches_mem(pattern, counts) =
+countmm(pattern, counts) =
     get!(mem, (pattern, counts)) do
-        countmatches(pattern, counts)
+        countm(pattern, counts)
     end
 
-function countmatches(pattern, counts)
-    reset()
-    now, later = counts[1], counts[2:end]
-    r = Regex("[#,?]{$(now)}[.?]")
-    matches = findall(r, pattern, overlap=true)
-    isempty(matches) && return 0
-    sum(first.(matches)) do i
-        any(==('#'), collect(pattern[1:i-1])) && return 0
-        next = pattern[i+now+1:end]
-        if isempty(later)
-            return !any(==('#'), collect(next))
-        end
-        return countmatches_mem(next, later)
+function countmatches_liner(pattern, counts)
+    if length(counts) == 0
+        any(==('#'), collect(pattern)) && return 0  # no more counts, but still #
+        return 1
+    elseif length(pattern) == 0 # not enough pattern left
+        return 0
+    elseif length(pattern) < length(counts) + sum(counts) - 1  # terminate early if not enough pattern left
+        return 0
     end
+    len, later = counts[1], counts[2:end]
+    c = pattern[1]
+    combs = 0
+
+    if c == '#' || c == '?' # we can start here
+        if length(pattern) >= len && # have enough space
+           !any(==('.'), collect(pattern[1:len])) && # dont cross dots
+           (length(pattern) == len || pattern[len+1] != '#') # and dont end before #
+            combs += countmm(pattern[len+2:end], later)
+        end
+    end
+    if c == '.' || c == '?' # we can start later
+        combs += countmm(pattern[2:end], counts)
+    end
+    return combs
 end
 
 function part1(lines)
+    reset()
     sum(parseinput(lines)) do (pattern, counts)
-        countmatches(pattern, counts)
+        countmm(pattern, counts)
     end
 end
 
+unfold(pattern, counts, n=5) = join((pattern[1:end] for i in 1:n), "?"), repeat(counts, outer=n)
+
 function part2(lines)
+    reset()
     sum(parseinput(lines)) do (pattern, counts)
-        pattern, counts = unfold(pattern, counts)
-        countmatches(pattern, counts)
+        countmm(unfold(pattern, counts)...)
     end
 end
 
 function test()
     @assert part1(sample) == 21
     @assert part1(data) == 7705
-    @assert @show(part2(data)) < 93822409359123
-
+    @assert part2(sample) == 525152
+    @assert part2(data) == 50338344809230
 end
-
-function unfold(pattern, counts, n=5)
-    pattern = join((pattern[1:end-1] for i in 1:n), "?") * "."
-    counts = repeat(counts, outer=n)
-    pattern, counts
-end
-
 
 sample = "???.### 1,1,3
 .??..??...?##. 1,1,3
