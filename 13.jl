f61673f4a7ed7a50e28acf80d4a6ca1feb66c71c
Sikorski <sikorski@zib.de>
2023-12-13 13:26:40 +0100

    prettier day13

--- 13.jl
+++ 13.jl
@@ -1,65 +1,29 @@
-parselines(lines) =
+function colsymdiff(col, block)
+  dist = min(col, size(block, 2) - col)
+  sum((block[:, col-dist+1:col] .!= block[:, col+dist:-1:col+1]))
+end
+
+findcolsym(b, t) = begin
+  x = [i for i in 1:size(b, 2)-1 if colsymdiff(i, b) == t]
+  return isempty(x) ? 0 : first(x)
+end
+
+symvalue(b, t=0) = 100 * findcolsym(b, t) + findcolsym(b', t)
+
+part1(input) = sum(symvalue, parseinput(input))
+part2(input) = sum(x -> symvalue(x, 1), parseinput(input))
+
+parseinput(lines) =
   map(split(lines, "\n\n")) do block
-    stack(map(split(block, "\n")) do line
-      collect(line) .== '#'
-    end)
+    stack(split(block)) .== '#'
   end
 
-function checksym(n, vec)
-  n >= length(vec) && return false
-  off = 0
-  while 0 < n - off < n + 1 + off <= length(vec)
-    vec[n-off] != vec[n+1+off] && return false
-    off += 1
-  end
-  return true
-end
-
-function symcount(block)
-  counts = Int[]
-  for n in 1:size(block, 1)-1
-    if checksym(n, eachrow(block))
-      push!(counts, n)
-    end
-  end
-  for n in 1:size(block, 2)-1
-    if checksym(n, eachcol(block))
-      push!(counts, n * 100)
-    end
-  end
-  isempty(counts) && return [0]
-  return counts
-end
-
-function smudge(block)
-  block = copy(block)
-  s = symcount(block)
-  for i in eachindex(block)
-    block[i] = !block[i]
-    s2 = symcount(block)
-    s2 = setdiff(s2, s)
-    if !isempty(s2)
-      return first(s2)
-    end
-    block[i] = !block[i]
-  end
-  return 0
-end
-
-function part1(input=data)
-  blocks = parselines(input)
-  sum(x -> first(symcount(x)), blocks)
-end
-
-function part2(input=data)
-  blocks = parselines(input)
-  sum(smudge, blocks)
-end
+###
 
 data = String(read("13.in"))
 ex = String(read("13.ex"))
 
 function test()
   @assert part1(data) == 41859
-  @assert part2(data) == 7178
+  @assert part2(data) == 30842
 end
